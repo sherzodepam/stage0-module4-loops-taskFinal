d76678910c40a7bef8b66372853e38d1495e8351
Name Surname <StudentID@newuzbekistanuniversity.uz>
2022-12-13 11:37:24 +0500

    do always

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
     }
 
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,10 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int temp=numberToPrint;
+        for (int i = 1; i < power ; i++) {
+            temp=numberToPrint*temp;
+        }
+        System.out.println(temp);
     }
 
     public static void main(String[] args) {
         new Power().printPower(10, 3);
+        System.out.println();
     }
 }
