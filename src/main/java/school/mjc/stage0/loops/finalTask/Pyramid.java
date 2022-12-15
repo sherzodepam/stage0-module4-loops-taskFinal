90ac042825445ef8e6dbe715612723279239cb06
Name Surname <StudentID@newuzbekistanuniversity.uz>
2022-12-15 14:21:19 +0500

    do always

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,25 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = cathetusLength; j >= 1; j--) {
+                if (j > i)
+                    System.out.print(" ");
+                else
+                    System.out.print(j);
+            }
+            for (int j = 2; j < cathetusLength + 1; j++) {
+                if (j <= i)
+                    System.out.print(j);
+                else if (i == cathetusLength)
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
+
     }
 }
